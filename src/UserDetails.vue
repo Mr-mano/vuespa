c417cab7f8caf65be08b565699ca08444c9fb4bb
Mr-mano <mano.quere@orange.fr>
2020-07-31 15:31:52 +0200

    first commit

---
+++ src/UserDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Détails de l'utilisateur {{this.$route.params.id}}</h2>
+        <p><router-link to="/users">Retour</router-link></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'user-details',
+    props: ['usr'],
+        methods:{
+            acceptInvitation: function(){
+                this.$emit('accept');
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/UserSummary.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="user-summary">
+        Id: {{usr.id}}, Ville: {{usr.address.city}}, CP: {{usr.address.zipcode}}, email: <router-link v-bind:to="/users/ + usr.id">{{usr.email}}</router-link>
+        <button v-on:click="acceptInvitation">Accepter</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'user-summary',
+    props: ['usr'],
+        methods:{
+            acceptInvitation: function(){
+                this.$emit('accept');
+        }
+    }
+}
+</script>
+
+<style>
+.user-summary{
+    border: 2px solid grey;
+    margin: 10px;
+    padding: 15px;
+    width: 250px;
+}
+</style>
